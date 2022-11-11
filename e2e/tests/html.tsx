2d14cb340480b83f21cc9d98917bff22ffa32899
Lukas Rosenfeldt <lukasrosenfeldt@gmail.com>
2022-11-11 14:53:50 +0100

    introduce jest for testing

--- e2e/tests/html.tsx
+++ e2e/tests/html.tsx
@@ -1,10 +1,15 @@
 import { stringify } from "@tsx-stringify/core";
-const html = (
-  <div role="tab">
-    <h1 class="heading-lg">Welcome to TypeScript-Land!</h1>
-    <section>
-      <h2 class="heading-md">How to: Custom JSX in TypeScript</h2>
-      <em>Look how awesome it is</em>
-    </section>
-  </div>
-);
+describe("jsx is html with xml-syntax", () => {
+  it("outputs the corresponding the html", () => {
+    const html = (
+      <div role="tab">
+        <h1 class="heading-lg">Welcome to TypeScript-Land!</h1>
+        <section>
+          <h2 class="heading-md">How to: Custom JSX in TypeScript</h2>
+          <em>Look how awesome it is</em>
+        </section>
+      </div>
+    );
+    expect(html).not.toBe("");
+  });
+});
---
+++ jest.config.js
@@ -0,0 +1,27 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+const config = {
+  verbose: true,
+  projects: [
+    {
+      displayName: "e2e: Test as tsx-stringify a package",
+      preset: "ts-jest",
+      rootDir: "./e2e",
+      testEnvironment: "jsdom",
+      testMatch: ["<rootDir>/tests/**/*.[jt]s?(x)"],
+    },
+    {
+      displayName: "core: Unit tests",
+      preset: "ts-jest",
+      rootDir: "./packages/core",
+      testEnvironment: "node",
+      testMatch: ["<rootDir>/tests/**/*.[jt]s?(x)"],
+      transform: {
+        "^.+\\.tsx?$": [
+          "ts-jest",
+          { tsconfig: "<rootDir>/tsconfig-test.json" },
+        ],
+      },
+    },
+  ],
+};
+module.exports = config;
